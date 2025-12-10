0993bc6cb6cca3ae4061250e362fdc581269ca02
visha <vishalsengardtu@gmail.com>
2025-12-10 18:29:42 +0530

    Best time to buy and sell stock I.

---
+++ Array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    // LINK - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    // Only 1 trade allowed.
+    public int maxProfitI(int[] prices) {
+        int profit = 0, mx = 0, mn = Integer.MAX_VALUE;
+
+        for(int price : prices) {
+            if(price < mn) {
+                mx = mn = price;
+            } else if(price  > mx) {
+                mx = price;
+                profit = Math.max(profit, mx - mn);
+            }
+        }
+
+        return profit;
+    }
+}
